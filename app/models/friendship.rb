5966e607164c1a96a0a3859897a04bc076227561
JavierSantaCruzXD <2010204javiergomez@gmail.com>
2020-10-21 23:50:34 -0600

    editar perfil

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,7 +4,6 @@ class Friendship < ApplicationRecord
 	belongs_to :friend, class_name: "User"
 	validates :user_id, uniqueness:{scope: :friend_id}
 
-
   def self.friends?(user, friend)
   	return true if user == friend
   	return Friendship.where(user:user, friend:friend).or(Friendship.where(user:friend, friend:user)).any?
